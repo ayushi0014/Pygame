87d4a4ab8bccfbf60d5fafe7722897f039e12b41
ayushi-sharma-123 <sharmaa14001@gmail.com>
2020-05-29 23:07:38 +0530

    Space Invader

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import pygame
+import math
 import random
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-done = False
+
 
 #background
 background = pygame.image.load('background.png')
@@ -21,7 +22,7 @@ playerX_change = 0
 
 #enemy
 enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0,100)
+enemyX = random.randint(0,735)
 enemyY = random.randint(50,150)
 enemyX_change = 4
 enemyY_change = 40
@@ -34,7 +35,7 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
-
+score = 0
 
 # player icon
 def player(x,y):
@@ -49,21 +50,31 @@ def fire_bullet(x,y):
     bullet_state = "fire"
     screen.blit(bulletImg, (x+16 , y+10))
 
-#main loop
-while not done: 
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(enemyX-bulletX, 2))+ (math.pow(enemyY-bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False    
+
+# game loop
+running = True
+while running: 
     screen.fill((0,0,0))
     screen.blit(background, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
+            running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)   
-                
+                if bullet_state is "ready": 
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)   
+                    
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -78,7 +89,7 @@ while not done:
     if enemyX <= 0:
         enemyX_change=4
         enemyY+= enemyY_change
-    elif enemyX >= 768:
+    elif enemyX >= 736:
         enemyX_change = -4
         enemyY+= enemyY_change
     
@@ -86,10 +97,20 @@ while not done:
     if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
-        
+
     if bullet_state is "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
+    
+    #collision
+    collision = isCollision(enemyX, enemyY, bulletX, bulletY)
+    if collision:
+        bulletY = 410
+        bullet_state = "ready"
+        score += 1
+        print(score)
+        enemyX = random.randint(0,100)
+        enemyY = random.randint(50,150)
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
