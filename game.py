4cde870705ca33d52a3039288bcd0845682e256e
ayushi-sharma-123 <sharmaa14001@gmail.com>
2020-05-29 19:04:19 +0530

    Space Invader

--- game.py
+++ game.py
@@ -26,6 +26,15 @@ enemyY = random.randint(50,150)
 enemyX_change = 4
 enemyY_change = 40
 
+#bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+
 
 # player icon
 def player(x,y):
@@ -34,6 +43,11 @@ def player(x,y):
 #enemy icon
 def enemy(x,y):
     screen.blit(enemyImg, (x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16 , y+10))
 
 #main loop
 while not done: 
@@ -47,6 +61,9 @@ while not done:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)   
+                
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -65,6 +82,15 @@ while not done:
         enemyX_change = -4
         enemyY+= enemyY_change
     
+    # movement of bullet
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+        
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
