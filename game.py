f5ce5c7228f60c41998c4132d97de3dfc86e9aae
ayushi-sharma-123 <sharmaa14001@gmail.com>
2020-05-29 12:05:14 +0530

    added enemy

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
@@ -9,20 +10,47 @@ pygame.display.set_caption("Space Invader")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-playerImg = pygame.image.load('spaceship.png')
+# player
+playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 410
+playerX_change = 0
 
+#enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0,100)
+enemyY = random.randint(50,150)
+enemyX_change = 0
+
+
+# player icon
 def player(x,y):
     screen.blit(playerImg, (x,y))
 
+#enemy icon
+def enemy(x,y):
+    screen.blit(enemyImg, (x,y))
 
+#main loop
 while not done: 
     screen.fill((0,0,0))
-
-    playerY -= 0.1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX=0
+    elif playerX >= 736:
+        playerX = 736
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
